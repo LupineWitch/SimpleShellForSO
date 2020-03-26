80e6aeab11c4d6b4d6ecd222fede737002befdcf
Vekz <V4nII@hotmail.com>
2020-03-26 14:53:38 +0100

    Reading line works and splits it by spaces

--- bash.c
+++ bash.c
@@ -5,72 +5,62 @@
 
 int cutWithSpace(char * input, char* command, char **parameters)
 {
-if(input == NULL) // If input is empty return error value -1
-{
-    return -1;
-}
-
-char* token = NULL ; // Setting up our word buffer
-token = strtok(input, " "); // Getting first word from input
-
-command = token ; // The first word in input is a command
-
-int i = 0 ; // Setting up loop variable
-while(token != NULL && i <1000) // While there is word and there aren't more than 1000 of them
-{
-    token  = strtok(NULL, " "); // Read the next word
-    if(token != NULL ) // If it exists
+    if(input == NULL) // If input is empty return error value -1
     {
-        parameters[i] = token ;  // Put it as next argument on our list
-        i++; // Keep track of loop iteration
+        return -1;
     }
-}
 
-return 0; // All good
+    char* prevSeq = NULL;
+    char* token = NULL; // Setting up our word buffer
+    token = strtok_r(input, " ", &prevSeq); // Getting first word from input
+
+    strncpy(command, token, 999); // The first word in input is a command
+
+    int i = 0 ; // Setting up loop variable
+    while(token != NULL && i <1000) // While there is word and there aren't more than 1000 of them
+    {
+        token  = strtok_r(NULL, " ", &prevSeq); // Read the next word
+        if(token != NULL ) // If it exists
+        {
+            parameters[i] = token ;  // Put it as next argument on our list
+            i++; // Keep track of loop iteration
+        }
+    }
+
+    return 0; // All good
 }
 
 
 int main()
 {
-char * cPath = getenv("PATH"); // Environment variable $PATH
-char * cDir; // Setting up variable for current directory path
-char * cUsr; // Setting up variable for current user
+    char* cPath = getenv("PATH"); // Environment variable $PATH
+    char* cDir; // Setting up variable for current directory path
+    char* cUsr; // Setting up variable for current user
 
-char command[1000]; // Command read from the user input
-char *parameters[1000]; // Arguments of our command read from the user input
-char *input = (char*)malloc(10000*sizeof(char)); // Setting up variable for users input
+    char command[1000]; // Command read from the user input
+    char* parameters[1000]; // Arguments of our command read from the user input
+    char* input = (char*)malloc(10000*sizeof(char)); // Setting up variable for users input
 
-int readFlag; // Setting up flag for correct splicing and reading operation
+    int readFlag; // Setting up flag for correct splicing and reading operation
 
-while(1)
-{
-    cDir = getenv("PWD"); // Getting current directory
-    cUsr = getenv("USER"); // Getting current user
-
-    printf("%s : %s >> ", cUsr, cDir); // Displaying command prompt
-
-    fgets(input, 10000, stdin); // Getting users input
-    readFlag = cutWithSpace(input, command, parameters); // Splitting users input by spaces on [command] [parameters]
-    if(readFlag == -1){ // If reading input was bad
-        printf("You provided wrong input!\n"); // Display error msg
-        continue; // Skip this loop iteration
-    } 
-
-    //Testing block #1
-    /*
-        TODO: 
-        CORRECT READING FROM LINE
-        PURGING THE COMMAND AND PARAMETERS LIST BETWEEN COMMANDS
-    */
-    printf("The command is %s\n", command); 
-    int i = 0;
-    while(parameters[i] != NULL)
+    while(1)
     {
-        printf("Parameter %d was %s\n", i, parameters[i]);
-        i++;
+        memset(command, 0,1000); // Clearing command
+        memset(parameters, 0, 1000); // Clearing parameters
+        memset(input, 0, 10000); // Clearing input
+        readFlag = 0;
+
+        cDir = getenv("PWD"); // Getting current directory
+        cUsr = getenv("USER"); // Getting current user
+        printf("%s : %s >> ", cUsr, cDir); // Displaying command prompt
+
+        fgets(input, 10000, stdin); // Getting users input
+        readFlag = cutWithSpace(input, command, parameters); // Splitting users input by spaces on [command] [parameters]
+        if(readFlag == -1){ // If reading input was bad
+            printf("You provided wrong input!\n"); // Display error msg
+            continue; // Skip this loop iteration
+        } 
     }
-}
 
-
-return 0 ;
+    return 0 ;
 }
