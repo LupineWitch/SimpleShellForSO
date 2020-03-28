ab6a6d7a4a522e079bf6610ca42f33280452f018
Wolfgangpolska <wolfgangpolska@gmail.com>
2020-03-28 15:56:40 +0100

    -implemented process forking and command execution -removed PATH variable (obsolete)

--- bash.c
+++ bash.c
@@ -1,9 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
+#include <unistd.h>
+#include <wait.h>
 
 
-int cutWithSpace(char * input, char* command, char **parameters)
+
+
+void forkAndExecute(char* args[])
+{
+
+    int ProcessID = fork(); //forking existing process
+
+    if(ProcessID == -1 ) // if fail
+    {
+        printf("Failed to fork");
+    }
+    else if(ProcessID == 0)
+    {
+         /* The execvp uses built array of arguments, the first argument being filename of program being run */
+      if( execvp(args[0],args) < 0 ) // chec if exec  failed  
+      {
+          printf("Could not execute command: %s ", args[0]);
+          int i = 1;
+          while(puts(args[i])>=0)
+          {
+              putchar(32);
+              i++;
+
+          }
+          exit(0); // Killing child 
+       
+      }
+    }
+    else
+    {
+        waitpid(ProcessID,NULL,0); // wait for child to finish
+    }
+    
+
+
+}
+
+int cutWithSpace(char * input, char **parameters)
 {
     if(input == NULL) // If input is empty return error value -1
     {
@@ -14,9 +53,9 @@ int cutWithSpace(char * input, char* command, char **parameters)
     char* token = NULL; // Setting up our word buffer
     token = strtok_r(input, " ", &prevSeq); // Getting first word from input
 
-    strncpy(command, token, 999); // The first word in input is a command
+    parameters[0] = token ; // First arg is a program name
 
-    int i = 0 ; // Setting up loop variable
+    int i = 1 ; // Setting up loop variable
     while(token != NULL && i <1000) // While there is word and there aren't more than 1000 of them
     {
         token  = strtok_r(NULL, " ", &prevSeq); // Read the next word
@@ -33,11 +72,10 @@ int cutWithSpace(char * input, char* command, char **parameters)
 
 int main()
 {
-    char* cPath = getenv("PATH"); // Environment variable $PATH
     char* cDir; // Setting up variable for current directory path
     char* cUsr; // Setting up variable for current user
-
-    char command[1000]; // Command read from the user input
+    char *p; // helper pointer for finding newline char in input
+   // char command[1000]; // Command read from the user input  - obsolete
     char* parameters[1000]; // Arguments of our command read from the user input
     char* input = (char*)malloc(10000*sizeof(char)); // Setting up variable for users input
 
@@ -45,9 +83,9 @@ int main()
 
     while(1)
     {
-        memset(command, 0,1000); // Clearing command
         memset(parameters, 0, 1000); // Clearing parameters
         memset(input, 0, 10000); // Clearing input
+        p = NULL; //clearing helper pointer
         readFlag = 0;
 
         cDir = getenv("PWD"); // Getting current directory
@@ -55,11 +93,19 @@ int main()
         printf("%s : %s >> ", cUsr, cDir); // Displaying command prompt
 
         fgets(input, 10000, stdin); // Getting users input
-        readFlag = cutWithSpace(input, command, parameters); // Splitting users input by spaces on [command] [parameters]
-        if(readFlag == -1){ // If reading input was bad
+        if( ( p=strchr( input,'\n') ) !=NULL ) //removing newline, thus cutting string after it
+             {
+                    *p= '\0';
+             }
+        readFlag = cutWithSpace(input, parameters); // Splitting users input by spaces on [command] [parameters]
+        if(readFlag == -1){ // If reading input was invalid
             printf("You provided wrong input!\n"); // Display error msg
             continue; // Skip this loop iteration
         } 
+        else
+        {
+            forkAndExecute(parameters);
+        }
     }
 
     return 0 ;
